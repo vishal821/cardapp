42b925dc49a1f916c2ea2e533b827e6e543fdace
Vishal Gill <root@localhost.localdomain>
2019-09-24 08:12:27 +0530

    project setup files

---
+++ navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from "./src/screens/login";
+
+class Navigation extends React.Component {
+
+    render() {
+      return <Login />;
+    }
+  }
+  
+  export default Navigation;
---
+++ src/screens/login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image,Text, View } from 'react-native';
+
+class Login extends React.Component {
+
+    render() {
+      return (
+          <View>
+              <Text>
+                  Hi Welcome
+              </Text>
+          </View>
+      );
+    }
+  }
+  
+  export default Login;
